c66c1eb3c6f3b331ef14f20b7c51e6ae85f7d20e
Elvio Toccalino <etoccalino@creativa77.com.ar>
2013-11-15 16:59:14 -0300

    using airbrite-js bindings

--- js/cart.js
+++ js/cart.js
@@ -1,14 +1,5 @@
 //
-// HTML5 Local Storage Support
-//
-
-(function () {try{if (!this.localStorage)if (this.globalStorage)try {this.localStorage=this.globalStorage}catch(e) {}else{var a=document.createElement("div");a.style.display="none";document.getElementsByTagName("head")[0].appendChild(a);if (a.addBehavior) {a.addBehavior("#default#userdata");var d=this.localStorage={length:0,setItem:function (b,d) {a.load("localStorage");b=c(b);a.getAttribute(b)||this.length++;a.setAttribute(b,d);a.save("localStorage")},getItem:function (b) {a.load("localStorage");b=c(b);return a.getAttribute(b)},
-removeItem:function (b) {a.load("localStorage");b=c(b);a.removeAttribute(b);a.save("localStorage");this.length=0},clear:function () {a.load("localStorage");for (var b=0;attr=a.XMLDocument.documentElement.attributes[b++];)a.removeAttribute(attr.name);a.save("localStorage");this.length=0},key:function (b) {a.load("localStorage");return a.XMLDocument.documentElement.attributes[b]}},c=function (a) {return a.replace(/[^-._0-9A-Za-z\xb7\xc0-\xd6\xd8-\xf6\xf8-\u037d\u37f-\u1fff\u200c-\u200d\u203f\u2040\u2070-\u218f]/g,
-"-")};a.load("localStorage");d.length=a.XMLDocument.documentElement.attributes.length}}}catch(e){}})();
-
-
-//
-// Requires jQuery, and benefits from the local storage
+// Requires jQuery and Airbrite.js
 //
 
 (function ($, localStorage) {
@@ -22,16 +13,8 @@ removeItem:function (b) {a.load("localStorage");b=c(b);a.removeAttribute(b);a.sa
     },
 
     //
-    // Namespace to identify cart objects in the local storage
-    namespace: 'cart',
-
-    //
     // Line items in the cart
-    items: [],
-
-    //
-    // Full price for the entire cart (no discounts applied)
-    fullPrice: 0,
+    order: null,
 
     //
     // Constructor
@@ -39,43 +22,13 @@ removeItem:function (b) {a.load("localStorage");b=c(b);a.removeAttribute(b);a.sa
       // Adapt the config
       $.extend(this.config, options);
 
-      // Load cart from local storage if exists
-      this.load();
+      // Initialize a new order
+      this.order = new Airbrite.Order();
+
+      // Full price for the entire cart (no discounts applied)
+      this.order.fullPrice = 0;
 
       this.config.log && console.log('window.cart component initialized');
-    },
-
-    //
-    // Load the line items from the local storage, if it exists
-    load: function () {
-      try {
-        var jsonItems = localStorage.getItem(cart.namespace + "_items");
-        var jsonItems = localStorage.getItem(cart.namespace + "_fullPrice");
-        if (jsonItems) {
-          cart.items = JSON.parse(jsonItems);
-          this.config.log && console.log('loaded cart data from local storage');
-        }
-      } catch(e) {}
-    },
-
-    //
-    // Save the line items to the local storage, if it exists
-    save: function () {
-      try {
-        localStorage.setItem(cart.namespace + "_items", JSON.stringify(cart.items));
-        localStorage.setItem(cart.namespace + "_fullPrice", JSON.stringify(cart.fullPrice));
-        this.config.log && console.log('saved cart data to local storage');
-      } catch(e) {}
-    },
-
-    //
-    // Clear the line items from the local storage, if it exists
-    clear: function () {
-      try {
-        localStorage.removeItem(cart.namespace + "_items");
-        localStorage.removeItem(cart.namespace + "_fullPrice");
-        this.config.log && console.log('cleared cart data in local storage');
-      } catch(e) {}
     },
 
     //
@@ -89,21 +42,83 @@ removeItem:function (b) {a.load("localStorage");b=c(b);a.removeAttribute(b);a.sa
     // Second param is a function of type function(err){...}
     addItem: function (item, callback) {
       this.config.log && console.log('window.cart: adding item to cart...');
-      if (item.sku === undefined) { callback('item has no SKU') ; return }
-      if (item.price === undefined) { callback('item has no price') ; return }
-      if (item.quantity === undefined) { callback('item has no quantity') ; return }
+
+      // Add item to the order
+      try {
+        this.order.addItem(item);
+      } catch (e) {
+        callback(e);
+        return;
+      }
 
       // Acculumulate the price of this item/s
-      this.fullPrice += item.price * item.quantity;
-      // Add to the line items.
-      this.items.push({
-        sku: item.sku,
-        quantity: item.quantity
-      });
+      this.order.fullPrice += item.price * item.quantity;
 
       this.config.log && console.log('window.cart: added item to the cart')
 
       callback(null);
+    },
+
+    //
+    // Remove item from Airbrite order
+    removeItem: function (item, callback) {
+      // Adjust the full price
+      this.order.fullPrice -= item.price * item.quantity;
+
+      this.config.log && console.log('window.cart: removing item with SKU: ' + item.sku);
+
+      // Remove item from the order
+      this.order.removeItem(item);
+
+      callback(null);
+    },
+
+    //
+    // Add credit/debit card information to the order
+    //
+    // Takes an object with card information:;
+    //  {
+    //    number: ‘4242424242424242’,
+    //    cvc: ‘892’,
+    //    exp_month: ‘11’,
+    //    exp_year: ‘16’,
+    //    amount: 123400,
+    //    currency: 'usd'
+    //  }
+    //
+    //  The callback takes an error param, which is null if there were no errors.
+    addPayment: function (ccard, callback) {
+      // Pass the card data through to Airbrite order
+      this.order.addPayment(ccard, function (response, message) {
+        // Failed registering card information
+        if (response === 'error') {
+          this.config.log && console.log(message);
+          callback(message);
+          return;
+        }
+
+        // Payment information was accepted
+        callback(null);
+      });
+    },
+
+    //
+    // Checkout the order to Airbrite
+    //
+    //  The callback takes an error param, which is null if there were no errors.
+    placeOrder: function (callback) {
+      // Submit the order to Airbrite
+      this.order.submit(function (response, message) {
+        // Failed registering card information
+        if (response === 'error') {
+          this.config.log && console.log(message);
+          callback(message);
+          return;
+        }
+
+        // Airbrite accepted the order
+        callback(null);
+      });
     }
   }
 
